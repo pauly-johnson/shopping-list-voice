968eaa051988b8cd5e58da17f99a9d7b899d07cc
pauly-johnson <paul.m.johnson1988@gmail.com>
2025-02-24 10:41:42 +1300

    styling

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <img className='background-image' src='../items.webp' alt=''/>
       <header className="App-header">
-        <h1>Welcome to the Shopping List App</h1>
+        <h1 style={{padding: '10px'}}>Welcome to the Shopping List App</h1>
         <p>Add items to your shopping list using voice or text input.</p>
       </header>
       <ShoppingList />
