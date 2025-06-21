bfab500c124ca970cc688fc1ccb924185c47649a
paul <paul.m.johnson1988@gmail.com>
2025-06-22 11:34:14 +1200

    header

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
     <div className="App">
       <div className="background-image"></div>
       <header className="App-header">
-        <h1 style={{padding: '10px'}}>Welcome to the Shopping List App</h1>
-        <p>Add items to your shopping list using voice or text input.</p>
+        <h1 style={{padding: '10px'}}>Welcome</h1>
+        <p>Add items to your list using voice or text input.</p>
       </header>
       <ShoppingList />
     </div>
